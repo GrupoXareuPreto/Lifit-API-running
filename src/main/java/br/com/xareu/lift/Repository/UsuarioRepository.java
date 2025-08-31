d501719d3800b8561148c614c44e03edca41c596
jjmonstro <jpcorreia1211@gmail.com>
2025-08-31 16:11:01 -0300

    teste

--- src/main/java/br/com/xareu/lift/Repository/UsuarioRepository.java
+++ src/main/java/br/com/xareu/lift/Repository/UsuarioRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByEmail(String email);
-
+    Usuario findBySenha(String senha);
 
     Usuario findByEmailContainingIgnoreCase(String email);
 

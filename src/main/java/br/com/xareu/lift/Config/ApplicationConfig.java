91960f4fce0f778a4aad8cd957eacd065fe2995c
jjmonstro <142437981+jjmonstro@users.noreply.github.com>
2025-10-16 16:52:09 -0300

    Adicionei imports no  applicationconfig
    Added imports for AuthenticationManager and AuthenticationConfiguration.

--- src/main/java/br/com/xareu/lift/Config/ApplicationConfig.java
+++ src/main/java/br/com/xareu/lift/Config/ApplicationConfig.java
@@ -1,10 +1,12 @@
 // src/main/java/br/com/xareu/lift/Config/ApplicationConfig.java
 package br.com.xareu.lift.Config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
 
 @Configuration
 public class ApplicationConfig {

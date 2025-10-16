3bac2a6ea6142986db5e2b99216e5ea2c79e9f99
jjmonstro <142437981+jjmonstro@users.noreply.github.com>
2025-10-16 16:48:38 -0300

    Adicionei @bean no authentication manager  na ApplicationConfig

--- src/main/java/br/com/xareu/lift/Config/ApplicationConfig.java
+++ src/main/java/br/com/xareu/lift/Config/ApplicationConfig.java
@@ -15,4 +15,9 @@ public class ApplicationConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }

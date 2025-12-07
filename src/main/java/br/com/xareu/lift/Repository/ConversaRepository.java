fcc0fc9ed0ee223c932c9e3882ab62b4ec55b4cd
jjmonstro <jpcorreia1211@gmail.com>
2025-12-07 20:30:27 -0300

    fix try

--- src/main/java/br/com/xareu/lift/Repository/ConversaRepository.java
+++ src/main/java/br/com/xareu/lift/Repository/ConversaRepository.java
@@ -16,6 +16,4 @@ public interface ConversaRepository extends JpaRepository<Conversa, Long> {
 
     List<Conversa> findAllByIntegrantes_Id(Long usuarioId);
     
-    @Query("SELECT c FROM Conversa c WHERE c.integrantes.size = 2 AND :usuario1 MEMBER OF c.integrantes AND :usuario2 MEMBER OF c.integrantes")
-    Optional<Conversa> findConversaEntreDoasUsuarios(@Param("usuario1") Usuario usuario1, @Param("usuario2") Usuario usuario2);
 }
